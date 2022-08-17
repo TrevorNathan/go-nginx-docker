073370a88ea8cf691626baea2ccacd7bfdd6ec97
TrevorNathan <trevoratwijukire@gmail.com>
2022-08-17 22:09:28 +0300

    ADD:Go script to run an api

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+func main()  {
+	router := mux.NewRouter()
+	router.HandleFunc("/", DoHealthCheck).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+func DoHealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, i'm a golang microservice")
+	w.WriteHeader(http.StatusAccepted) //RETURN HTTP CODE 202
+}
